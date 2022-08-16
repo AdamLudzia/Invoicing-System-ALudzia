c5dd6fb546fecf8341d432a44e5cd0e110d1309a
adam.ludzia <adam.ludzia@gmail.com>
2022-08-16 21:20:05 +0200

    invoicing-system project recreated in spring

---
+++ src/main/java/com/adamludzia/InvoicingSystemAdamLudziaApplication.java
@@ -0,0 +1,13 @@
+package com.adamludzia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicingSystemAdamLudziaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoicingSystemAdamLudziaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adamludzia/InvoicingSystemAdamLudziaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adamludzia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicingSystemAdamLudziaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

c18e2f45b96a346e841b819c416c201356ba889f
adam.ludzia <adam.ludzia@gmail.com>
2022-08-17 13:20:57 +0200

    task 3 implementation and .GitHub/workflows configuration

---
+++ src/main/java/com/adamludzia/service/JsonService.java
@@ -0,0 +1,41 @@
+package com.adamludzia.service;
+
+import com.adamludzia.model.Invoice;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Service
+public class JsonService {
+
+    private ObjectMapper objectMapper;
+
+    {
+        objectMapper = new ObjectMapper();
+        objectMapper.findAndRegisterModules();
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    }
+
+    public String invoiceAsJson(Invoice invoice) {
+        try {
+            return objectMapper.writeValueAsString(invoice);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Service error: failed to convert string to JSON", e);
+        }
+    }
+
+    public <T> T returnJsonAsInvoice(String json, Class<T> invoice) {
+        try {
+            return objectMapper.readValue(json, invoice);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Service error: failed to parse JSON", e);
+        }
+
+    }
+
+}

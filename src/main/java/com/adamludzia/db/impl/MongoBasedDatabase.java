452bf55959722e61141c5087327b4710fa4064e1
adam.ludzia <adam.ludzia@gmail.com>
2022-09-12 14:30:51 +0200

    implementations - task 14

---
+++ src/main/java/com/adamludzia/db/impl/MongoBasedDatabase.java
@@ -0,0 +1,62 @@
+package com.adamludzia.db.impl;
+
+import com.adamludzia.db.Database;
+import com.adamludzia.model.Invoice;
+import com.adamludzia.service.MongoIdService;
+import com.mongodb.client.MongoCollection;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+import lombok.AllArgsConstructor;
+import org.bson.Document;
+
+
+    @AllArgsConstructor
+    public class MongoBasedDatabase implements Database {
+
+        private MongoCollection<Invoice> invoices;
+        private MongoIdService idService;
+
+        @Override
+        public long save(Invoice invoice) {
+
+            invoice.setId(idService.getNextIdAndIncrement());
+            invoices.insertOne(invoice);
+
+            return invoice.getId();
+        }
+
+        @Override
+        public Optional<Invoice> getById(long id) {
+            return Optional.ofNullable(
+                invoices.find(idFilter(id)).first()
+            );
+        }
+
+        @Override
+        public List<Invoice> getAll() {
+            return StreamSupport
+                .stream(invoices.find().spliterator(), false)
+                .collect(Collectors.toList());
+        }
+
+        @Override
+        public Optional<Invoice> update(long id, Invoice updatedInvoice) {
+            updatedInvoice.setId(id);
+            return Optional.ofNullable(
+                invoices.findOneAndReplace(idFilter(id), updatedInvoice)
+            );
+        }
+
+        @Override
+        public Optional<Invoice> delete(long id) {
+            return Optional.ofNullable(
+                invoices.findOneAndDelete(idFilter(id))
+            );
+        }
+
+        private Document idFilter(long id) {
+            return new Document("_id", id);
+        }
+    }
---
+++ src/main/java/com/adamludzia/service/MongoIdService.java
@@ -0,0 +1,4 @@
+package com.adamludzia.service;
+
+public class MongoIdService {
+}

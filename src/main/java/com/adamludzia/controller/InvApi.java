a2a4823995abd6c486aab354af0ec67831ac143f
adam.ludzia <adam.ludzia@gmail.com>
2022-09-13 13:00:51 +0200

    implementations - task 15

--- src/main/java/com/adamludzia/controller/InvApi.java
+++ src/main/java/com/adamludzia/controller/InvApi.java
@@ -2,6 +2,7 @@ package com.adamludzia.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import java.sql.SQLException;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -23,17 +24,17 @@ public interface InvApi {
 
     @ApiOperation(value = "Get list of all invoices")
     @GetMapping(produces = {"application/json;charset=UTF-8"})
-    List<Invoice> getAll();
+    List<Invoice> getAll() throws SQLException;
 
     @ApiOperation(value = "Get invoice by id")
     @GetMapping("/{id}")
-    ResponseEntity<Invoice> getById(@PathVariable int id);
+    ResponseEntity<Invoice> getById(@PathVariable int id) throws SQLException;
 
     @ApiOperation(value = "Update invoice with given id")
     @PutMapping("/{id}")
-    ResponseEntity<?> update(@PathVariable int id, @RequestBody Invoice invoice);
+    ResponseEntity<?> update(@PathVariable int id, @RequestBody Invoice invoice) throws SQLException;
 
     @ApiOperation(value = "Delete invoice with given id")
     @DeleteMapping("/{id}")
-    ResponseEntity<?> deleteById(@PathVariable int id);
+    ResponseEntity<?> deleteById(@PathVariable int id) throws SQLException;
 }

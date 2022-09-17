4ab5f503426f2e5caf930340de061d1b73fd1c0b
adam.ludzia <adam.ludzia@gmail.com>
2022-09-17 22:18:24 +0200

    implementations - checkstyle corrections #16

--- src/main/java/com/adamludzia/model/Car.java
+++ src/main/java/com/adamludzia/model/Car.java
@@ -2,8 +2,8 @@ package com.adamludzia.model;
 
 import io.swagger.annotations.ApiModelProperty;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import lombok.AllArgsConstructor;
